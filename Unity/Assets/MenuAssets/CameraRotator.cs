dae2f139c33546776bf46ee9ceb3f7f29bbbafb5
Yuye <47743107+Rugiacreed@users.noreply.github.com>
2020-10-13 09:48:50 +0200

    main menu scene created

---
+++ Unity/Assets/MenuAssets/CameraRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float cameraspeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, cameraspeed * Time.deltaTime, 0);
+    }
+}
