5ddcf111f8536c3c42ff0d0a28a86787f88fd910
Yuye <47743107+Rugiacreed@users.noreply.github.com>
2020-10-24 18:49:21 +0200

    design changes on interface

---
+++ Unity/Assets/MenuAssets/SelectAnimation.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class SelectAnimation : MonoBehaviour
+{
+    [SerializeField] private Vector3 selectionposition;
+    private Vector3 initialposition;
+    private void Awake()
+    {
+        initialposition = transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, selectionposition, 0.1f);
+
+    }
+
+    private void OnDisable()
+    {
+        transform.position = initialposition;
+    }
+}
